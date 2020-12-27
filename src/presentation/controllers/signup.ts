c1a0249af5cc98f0468fc905c9bee08cf96c419f
ISAQUE SOUZA RAMOS <isaque.ramos.ext@dasa.com.br>
2020-12-27 20:53:18 -0300

    refactor: move all protocols import to a single file

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,8 +1,6 @@
-import { HttpRequest, HttpResponse } from '../protocols/http'
 import { MissingParamError, InvalidParamError } from '../erros'
 import { badRequest, serverError } from '../helpers/helper-http'
-import { Controller } from '../protocols/controllers'
-import { EmailValidator } from '../protocols/email-validator'
+import { Controller, EmailValidator, HttpRequest, HttpResponse } from '../protocols'
 
 export class SignUpController implements Controller {
   private readonly emailValidator: EmailValidator
